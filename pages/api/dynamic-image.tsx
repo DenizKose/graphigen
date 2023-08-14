a43af0f443aa24c6505fed6250bd42b6b5adceb4
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-14 21:42:55 +0530

    Added type param

--- pages/api/dynamic-image.tsx
+++ pages/api/dynamic-image.tsx
@@ -17,6 +17,7 @@ export default async function handler(req: NextRequest) {
     month: 'long',
     day: 'numeric',
   })
+  const type = searchParams.get('type') || 'blog'
 
   return new ImageResponse(
     (
