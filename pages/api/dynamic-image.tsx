93b545ec36ccc6b7a43128b4a9bc2b16fd1bb9e6
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-14 20:50:41 +0530

    Added query params to the dynamic og

--- pages/api/dynamic-image.tsx
+++ pages/api/dynamic-image.tsx
@@ -7,24 +7,22 @@ export const config = {
 
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl
-  const username = searchParams.get('username')
-  if (!username) {
-    return new ImageResponse(<>{'Visit with "?username=vercel"'}</>, {
-      width: 1200,
-      height: 630,
-    })
-  }
+  const author = searchParams.get('author')
+  const title = searchParams.get('title')
+  const description = searchParams.get('description')
+  const image = searchParams.get('image')
+  const date = searchParams.get('date') || new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
 
   return new ImageResponse(
     (
       <div tw="bg-gray-100 flex w-full justify-center items-center h-screen">
         <div tw="max-w-sm rounded-lg overflow-hidden shadow-md bg-white flex">
           <div tw="flex flex-col p-4">
-              <span tw="text-gray-500 text-xs">August 14, 2023</span>
-              <h2 tw="text-lg font-semibold mb-1 mt-0">Introducing Line Range Filtering in Rails 7.1 Testing</h2>
-              <p tw="text-gray-600 text-sm mb-4">Rails 7.1 introduces line range filtering for running specific tests within a test file based on line numbers.</p>
+              <span tw="text-gray-500 text-xs">{date}</span>
+              <h2 tw="text-lg font-semibold mb-1 mt-0">{title}</h2>
+              <p tw="text-gray-600 text-sm mb-4">{description}</p>
               <div tw="flex justify-between items-center">
-                  <span tw="text-gray-700 text-xs font-medium">Akhil G Krishnan</span>
+                  <span tw="text-gray-700 text-xs font-medium">{author}</span>
                   <img src="https://avatars.githubusercontent.com/u/47183150?v=4g" tw="rounded-full" height="30" width="30"/>
               </div>
           </div>
